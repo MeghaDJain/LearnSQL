a8ccb69bf2cb7c5a608239a7333e56d84f274f65
MeghaDJain <116029044+MeghaDJain@users.noreply.github.com>
2024-03-12 21:14:28 +0530

    Create_Inserts_WomenWardrobe.sql

---
+++ Create_Inserts_WomenWardrobe.sql
@@ -0,0 +1,103 @@
+CREATE TABLE WomenWardrobe (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  category VARCHAR(50) NOT NULL,
+  item_name VARCHAR(100) NOT NULL,
+  brand VARCHAR(50),
+  color VARCHAR(50),
+  size VARCHAR(10),
+  occasion VARCHAR(50));
+  
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Little Black Dress', 'Calvin Klein', 'Black', '8', 'Formal');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Floral Sundress', 'Zara', 'Multicolor', 'M', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Silk Blouse', 'Equipment', 'Ivory', 'S', 'Work');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Cashmere Sweater', 'Everlane', 'Gray', 'M', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Bottoms', 'Jeans', 'Madewell', 'Blue', '28', 'Everyday');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Shoes', 'Loafers', 'Sam Edelman', 'Tan', '7.5', 'Work');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Shoes', 'Sneakers', 'Veja', 'White', '8', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Coats', 'Trench Coat', 'Burberry', 'Beige', 'M', 'Spring/Fall');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Outerwear', 'Puffer Jacket', 'North Face', 'Black', 'S', 'Winter');
+
+
+
+-- Dresses (10)
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Wrap Dress', 'Diane von Furstenberg', 'Blue', '8', 'Work/Wedding Guest');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Midi Dress', ' Reformation', 'Navy Blue', '10', 'Work/Wedding Guest');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Little Black Dress', 'Calvin Klein', 'Black', '6', 'Formal');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Floral Sundress', 'Zara', 'Multicolor', 'M', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Slip Dress', 'Réalisation (Brand Name)', 'Black', 'S', 'Night Out');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Denim Shirt Dress', 'Everlane', 'Light Wash', 'M', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Maxi Dress', 'Boden', 'Floral Print', '12', 'Summer');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Sweater Dress', 'Madewell', 'Maroon', 'S', 'Fall/Winter');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'T-Shirt Dress', 'COS', 'White', 'M', 'Everyday');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Jumpsuit', 'Theory', 'Navy Blue', '8', 'Work/Formal');
+
+-- Tops (10)
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Silk Blouse', 'Equipment', 'Ivory', 'S', 'Work');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Turtleneck Sweater', 'Uniqlo', 'Black', 'M', 'Work/Winter');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Cashmere Sweater', 'Everlane', 'Gray', 'M', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'T-Shirt', 'Everlane', 'White', 'S', 'Everyday');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Tank Top', 'Aritzia', 'Striped', 'M', 'Summer');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Button-Down Shirt', 'J.Crew', 'Blue', '8', 'Work/Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Sweatshirt', 'Champion', 'Gray', 'L', 'Casual');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Blazer', 'Zara', 'Black', 'M', 'Work/Formal');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Camisole', 'H&M', 'Floral Print', 'S', 'Summer');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Tops', 'Long-Sleeve Tee', 'Uniqlo', 'White', 'M', 'Everyday');
+
+INSERT INTO WomenWardrobe (category, item_name, brand, color, size, occasion)
+VALUES ('Dresses', 'Sequin Mini Dress', ' & Other Stories', 'Silver', '8', 'Party/New Year\'s Eve');
+
